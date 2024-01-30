69eaa4cd6876e2e0155f0c7bafba8993296df37f
Evi Siafaka <evi.siafaka@hotmail.com>
2024-01-30 12:53:33 +0100

    Re-commited everything I have no idea what I am doing but I'll get there

--- middleware/advancedAuth.js
+++
@@ -1,9 +0,0 @@
-import { auth } from 'express-oauth2-jwt-bearer';
-
-const authMiddleware = auth({
-    audience: 'https://my_booking_app',
-    issuerBaseURL: `https://dev-qba48wbjjco1tcit.eu.auth0.com/`,
-    tokenSigningAlg: 'RS256'
-});
-
-export default authMiddleware;
